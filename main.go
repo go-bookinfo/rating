34a7ca1f0d3ca9dc5a228b7820f2098d835aaa0d
gustjd887 <gustjd887@gmail.com>
2023-03-26 10:43:46 +0900

    jeager 설정 추가

--- main.go
+++ main.go
@@ -1,20 +1,54 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/sdk/resource"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
 
 const (
 	DB_USER     = "postgres"
 	DB_PASSWORD = "postgres"
 	DB_NAME     = "bookinfo"
+
+	service     = "trace-demo"
+	environment = "production"
+	id          = 1
 )
 
 type ratings struct {
 	Id   int `json:"Id"`
 	Star int `json:"Star"`
+}
+
+func tracerProvider(url string) (*tracesdk.TracerProvider, error) {
+	// Create the Jaeger exporter
+	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+	if err != nil {
+		return nil, err
+	}
+	tp := tracesdk.NewTracerProvider(
+		// Always be sure to batch in production.
+		tracesdk.WithBatcher(exp),
+		// Record information about this application in a Resource.
+		tracesdk.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName(service),
+			attribute.String("environment", environment),
+			attribute.Int64("ID", id),
+		)),
+	)
+	return tp, nil
 }
 
 func main() {
@@ -33,9 +67,37 @@ func main() {
 		fmt.Println(err)
 	}
 
+	// start
+	tp, err := tracerProvider("http://localhost:32293/api/traces")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Register our TracerProvider as the global so any imported
+	// instrumentation in the future will default to using it.
+	otel.SetTracerProvider(tp)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Cleanly shutdown and flush telemetry when the application exits.
+	defer func(ctx context.Context) {
+		// Do not make the application hang when it is shutdown.
+		ctx, cancel = context.WithTimeout(ctx, time.Second*5)
+		defer cancel()
+		if err := tp.Shutdown(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}(ctx)
+
+	tr := tp.Tracer("component-main")
+
+	ctx, span := tr.Start(ctx, "rating")
+	defer span.End()
+
 	http.HandleFunc("/rating", func(w http.ResponseWriter, r *http.Request) {
 		w.Write(bs)
 	})
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8000", nil)
 
 }
